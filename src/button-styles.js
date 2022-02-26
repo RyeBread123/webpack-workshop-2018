910791ced314d8a8da3092fa60153de37c79aaf3
Ryder Wendt <ryderw93@gmail.com>
2022-02-25 21:29:38 -0500

    webpack step 3

---
+++ src/button-styles.js
@@ -0,0 +1,9 @@
+const red = "color:red";
+const blue = "color : blue";
+const makeColorStyle = color => `color: ${color}`;
+
+// exports.red = red;
+// exports.blue = blue;
+// exports.makeColor = makeColorStyle;
+
+export {red, blue, makeColorStyle}
---
+++ src/button.js
@@ -0,0 +1,6 @@
+// take a string, the button label and return an element
+const makeButton = buttonName => {
+  return `Button : ${buttonName}`;
+};
+
+module.exports = makeButton;
